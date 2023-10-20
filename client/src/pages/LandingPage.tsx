1e09189c5cf3fdb0a9c6fdf5da90d38b795bd411
alaminShaheen <As2022!!@@>
2023-10-20 16:38:06 -0400

    chore: Beautification of landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button.tsx";
+
 const LandingPage = () => {
 	return (
-		<div className="text-7xl font-bold text-blue-700 h-screen flex justify-center items-center">
+		<div className="text-7xl font-bold bg-zinc-900 text-blue-700 h-screen flex flex-col justify-center items-center">
 			<span>Landing page of LU Folks</span>
+			<div>
+				<Link to="/login" className="underline underline-offset-4 hover:text-primary">
+					<Button variant="secondary">Sign in!</Button>
+				</Link>
+				<Link
+					to="/register"
+					className="underline underline-offset-4 hover:text-primary ml-3"
+				>
+					<Button variant="secondary">Sign up!</Button>
+				</Link>
+			</div>
 		</div>
 	);
 };
