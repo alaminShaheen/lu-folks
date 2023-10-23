962eccacdaa3c9f87cce664181e570e5f75753c2
alaminShaheen <As2022!!@@>
2023-10-23 01:08:45 -0400

    chore: updated environment config for google oauth 2.0

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -4,13 +4,21 @@ function validateEnv() {
 	cleanEnv(process.env, {
 		NODE_ENV: str({ choices: ["development", "production"], default: "development" }),
 		PORT: port({ default: 1337 }),
+
 		POSTGRES_HOST: str({ default: "localhost" }),
 		POSTGRES_USER: str(),
 		POSTGRES_PASSWORD: str(),
 		POSTGRES_DATABASE_PORT: port({ default: 5432 }),
 		POSTGRES_DATABASE_NAME: str({ default: "lu-folks-db-dev" }),
+
 		ACCESS_TOKEN_SECRET: str(),
 		REFRESH_TOKEN_SECRET: str(),
+
+		CLIENT_ORIGIN_URL: str(),
+
+		GOOGLE_OAUTH_CLIENT_ID: str(),
+		GOOGLE_OAUTH_CLIENT_SECRET: str(),
+		GOOGLE_OAUTH_REDIRECT_URL: str(),
 	});
 }
 
