80b597547f3cd97227d67bb12ddd640679244114
alaminShaheen <As2022!!@@>
2023-10-27 22:39:09 -0400

    chore: minor logo fix

--- client/src/components/AppLogo.tsx
+++ client/src/components/AppLogo.tsx
@@ -1,6 +1,20 @@
+import { ImgHTMLAttributes } from "react";
+import { cn } from "@/lib/utils.ts";
 
-const AppLogo = () => {
-	return <img src="/logo-transparent-svg.svg" alt="" />;
+interface AppLogoProps extends Omit<ImgHTMLAttributes<HTMLImageElement>, "src" | "alt"> {
+	size?: number;
+}
+
+const AppLogo = (props: AppLogoProps) => {
+	const { size = 11, className, ...rest } = props;
+	return (
+		<img
+			className={cn(className, `h-${size} w-${size}`)}
+			src="/logo-transparent-svg.svg"
+			alt="app logo"
+			{...rest}
+		/>
+	);
 };
 
 export default AppLogo;
